0efe9255f034fac996838b00eb71e770f365a606
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-24 15:18:28 +0300

    add Console App

---
+++ ConsoleBooksApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksCollection;
+
+namespace ConsoleBooksApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookCollection collection = new BookCollection();
+            Book book = new Book("Title", "author", "genre", DateTime.Now, 150, 20);
+            collection.AddBook(book);
+            collection.SaveBooks();
+            BookCollection resCollection = new BookCollection();
+            File.Create("Collection");
+            FileInfo file = new FileInfo("Collection");
+            IBooksStorage st = new BinaryFileStorage();
+            
+            resCollection.LoadBooks();
+
+        }
+    }
+}
---
+++ ConsoleBooksApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ConsoleBooksApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleBooksApp")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2d4f321c-8870-446a-b79b-d94cddc49b5c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
